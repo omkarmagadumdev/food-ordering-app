bef676be77db689492de0d1464e20797b8c978c6
Omkar Magadum <omkar.magadum.dev@gmail.com>
2025-12-12 21:39:03 +0530

    chore: initialize package.json for food ordering app with dependencies and scripts

--- App.js
+++ App.js
@@ -1,4 +1,23 @@
- const heading = React.createElement("h1", { id:"heading" },"hello world!!");
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading)
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "i am a nested h1 tag"),
+      React.createElement("h2", {}, "i am a nested h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "i am a nested h1 tag"),
+      React.createElement("h2", {}, "i am a nested h2 tag"),
+    ])
+  ]
+);
+
+// App component return karega parent element
+export default function App() {
+  return parent;
+}

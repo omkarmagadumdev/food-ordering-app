ddc8842d35fdddb617d3d7290df28d246ee49a6b
Omkar Magadum <omkar.magadum.dev@gmail.com>
2025-12-19 20:36:02 +0530

    feat: add rimraf for cleanup in package.json; implement fetchData function in Body component to retrieve restaurant data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,43 @@
 
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
   // Extract ALL restaurants
   const allRestaurants = resList.data.cards
-    .filter((c) => c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant")
-    .map((c) => c?.card?.card?.info);
+  .filter((c) => c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant")
+  .map((c) => c?.card?.card?.info);
+  const [Restaurant, setAllRestaurants] = useState(allRestaurants);
+
+  useEffect(()=>{
+    fetchData()
+    
+  },[])
+
+  const fetchData = async ()=>{
+   const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9254533&lng=77.546757&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null")
+
+   const json = await data.jason();
+   console.log(json);
+   
+  }
 
   return (
     <div className="body">
       <div className="filter">
         <button className="filterbtn" onClick={() => {
-          const filtered = allRestaurantsData.filter((res) => res.avgRating > 4.5);
+          const filtered = allRestaurants.filter((res) =>{
+            const rating = Number(res?.avgRating ?? res?.avgRatingString)
+            return !Number.isNaN(rating) && rating > 4.5
+          });
           setAllRestaurants(filtered);
         }}>
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {allRestaurants.map((restaurant) => (
+        {Restaurant.map((restaurant) => (
             
           <RestaurantCard   
             key={restaurant?.id}

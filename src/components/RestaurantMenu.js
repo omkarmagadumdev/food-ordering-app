6152ebd48456218982b951838da55f7c16d1d681
Omkar Magadum <omkar.magadum.dev@gmail.com>
2025-12-28 21:48:43 +0530

    feat: enhance restaurant menu styling and update data fetching logic; add new CSS classes for menu items and improve loading state handling

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,14 +16,14 @@ const RestaurantMenu = () => {
         const data = await fetch("https://proxy.corsfix.com/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9254533&lng=77.546757&restaurantId=576474&catalog_qa=undefined&query=Biryani&submitAction=ENTER")
 
         const json = await data.json();
-        setresInfo(json.data)
+        setresInfo(json.menucard)
     }
     
     
     
-    if(resInfo === null ) <Shimmer />
+    if(resInfo === null ) return <Shimmer />
     
-    const {itemcards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card?.categories[0]?.itemCards[0]
+    const itemcards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card?.items
 
     console.log(itemcards); 
     
